5a790374874b38d3da264f58ee6447dedb5998f4
Ino Tech <ezekieldavid150@gmail.com>
2025-12-16 00:39:05 +0100

    refactor: remove unused files and update App component for step navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [steps, setSteps] = useState(1);
+
+  function handlePrevious() {
+    if (steps > 1) {
+      setSteps(steps - 1);
+    }
+  }
+
+  function handleNext() {
+    if (steps < 3) {
+      setSteps(steps + 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="steps">
+    <div className="numbers">
+      <div className={steps >= 1 ? "active" : ""}>1</div>
+      <div className={steps >= 2 ? "active" : ""}>2</div>
+      <div className={steps >= 3 ? "active" : ""}>3</div>
     </div>
+
+    <p className="message">Steps {steps}: {messages[steps - 1]}</p>
+
+    <div className="buttons">
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} disabled={steps === 3}>Next</button>
+    </div>
+  </div>
   );
 }
-
-export default App;
