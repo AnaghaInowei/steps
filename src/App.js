eb133d2b6e848a8bdd6f081d55e91c9099b7eff5
Ino Tech <ezekieldavid150@gmail.com>
2025-12-16 12:07:57 +0100

    feat: add close button and toggle functionality for steps component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [steps, setSteps] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (steps > 1) {
@@ -22,7 +23,10 @@ export default function App() {
   }
 
   return (
-  <div className="steps">
+    <div>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
+  {isOpen && (
+    <div className="steps">
     <div className="numbers">
       <div className={steps >= 1 ? "active" : ""}>1</div>
       <div className={steps >= 2 ? "active" : ""}>2</div>
@@ -35,6 +39,7 @@ export default function App() {
       <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
       <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} disabled={steps === 3}>Next</button>
     </div>
+  </div>)}
   </div>
   );
 }
